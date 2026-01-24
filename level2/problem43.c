9b8c935696f2a2d8050b4d30712832822fe9ec5b
louazzanimaryem <maryemlouazzani@gmail.com>
2026-01-24 22:25:55 +0100

    count even

---
+++ level2/problem43.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int     rand_num(int from, int to)
+{
+    int rand_n = rand() % (to - from + 1) + from;
+    return(rand_n);
+}
+void    read_input(int *n)
+{
+    printf("entre a positive number\n");
+    scanf("%d", n);
+}
+void    fill_array(int arr[100], int size)
+{
+    int i =0;
+    while(i < size)
+    {
+        arr[i] = rand_num(1, 100);
+        i++;
+    }
+}
+void    print_array(int arr[100], int size)
+{
+    int i = 0;
+    while(i < size)
+    {
+        printf("%d ", arr[i]);
+        i++;
+    }
+}
+int     count_even_array(int arr[100], int size)
+{
+    int i = 0;
+    int count = 0;
+    while(i < size)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            count++;
+        }
+        i++;
+    }
+    return (count);
+}
+int main()
+{
+    srand((unsigned)time(NULL));
+    int arr[100];
+    int size;
+    read_input(&size);
+     printf("array  elements :\n");
+     fill_array(arr, size);
+     print_array(arr, size);
+     printf("\n");
+     int num = count_even_array(arr, size);
+     printf("even numbers count is : %d\n", num);
+     return 0;
+}

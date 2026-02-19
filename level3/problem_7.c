87c2fdb1ba69f29469e1c8ee257875dce727baac
louazzanimaryem <maryemlouazzani@gmail.com>
2026-02-19 02:31:57 +0000

    done

---
+++ level3/problem_7.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+void    fiLL_array_order(int arr[3][3], int row, int col)
+{
+    int i = 0;
+    int j = 0;
+    int num = 1;
+    while(i < row)
+    {
+        j = 0;
+        while(j < col)
+        {
+            arr[i][j] = num;
+            num = num + 1;
+            j++;
+        }
+        i++;
+    }
+}
+void    print_array(int arr[3][3], int row, int col)
+{
+    int i = 0;
+    int j = 0;
+    while(i < row)
+    {
+        j = 0;
+        while(j < col)
+        {
+            printf("%3d ", arr[i][j]);
+            j++;
+        }
+        printf("\n");
+        i++;
+    }
+}
+void    transpose(int arr[3][3], int row, int col)
+{
+    int i = 0;
+    int j = 0;
+    while(i < row)
+    {
+        j = 0;
+        while(j < col)
+        {
+            printf("%3d ", arr[j][i]);
+            j++;
+        }
+        printf("\n");
+        i++;
+    }
+}
+int main()
+{
+    int arr[3][3];
+    printf("the following is a 3*3 ordered matrix :\n");
+    fiLL_array_order(arr, 3, 3);
+    print_array(arr, 3, 3);
+    printf("the following is the transposed matrix :\n");
+    transpose(arr, 3, 3);
+}
